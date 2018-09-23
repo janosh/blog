2ed3fa31bb734b9133ebba565195142530045d2f
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-09-23 13:09:56 +0200

    feat: add background image to PageTitle

--- src/components/PageTitle/styles.js
+++ src/components/PageTitle/styles.js
@@ -1,7 +1,25 @@
 import styled from 'styled-components'
+import Img from 'gatsby-image'
 
 export const Title = styled.header`
-  margin-top: calc(2em + 4vh);
-  border-bottom: 1px solid gray;
+  margin-bottom: calc(2em + 2vh);
+  grid-column: 2 / -2;
+  grid-row: 1;
   text-align: center;
+  color: ${props => props.theme.mainWhite};
+  font-size: calc(1em + 1vw);
+  align-self: center;
+`
+
+export const Background = styled(Img)`
+  margin-bottom: calc(2em + 2vh);
+  z-index: -1;
+  grid-row: 1;
+  height: 40vh;
+  top: 0;
+  left: 0;
+  grid-column: 1 / -1 !important;
+  img {
+    object-position: bottom !important;
+  }
 `
