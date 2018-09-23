6730ad3e5e182c60598a72389b0eaf320f47d096
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-09-23 13:04:14 +0200

    style: CategoryList

--- src/components/CategoryList/styles.js
+++ src/components/CategoryList/styles.js
@@ -2,13 +2,11 @@ import styled from 'styled-components'
 import { Link } from 'gatsby'
 
 export const CategoryLink = styled(Link)`
-  display: flex;
-  width: max-content;
-  padding: 0 0.5rem;
-  margin: 0 1rem 1rem 0;
-  color: ${props => props.theme.darkGray} !important;
+  padding: 0 0.5em;
+  margin-right: 1em;
+  color: ${props => props.theme.darkGray};
   background: ${props => props.theme.lightGray};
-  border-radius: ${props => props.theme.mediumBorderRadius};
+  border-radius: ${props => props.theme.smallBorderRadius};
   &.${props => props.activeClassName} {
     background: ${props => props.theme.mainOrange};
   }
@@ -17,5 +15,5 @@ export const CategoryLink = styled(Link)`
 export const List = styled.div`
   display: flex;
   flex-wrap: wrap;
-  margin-bottom: 2rem;
+  margin-bottom: 1em;
 `
