5c579538710b53565d2ebbc860a43e47ebc905e7
JanoshRiebesell <janosh.riebesell@gmail.com>
2018-10-13 10:20:59 +0100

    style: PostTitle

--- src/components/PostTitle/styles.js
+++ src/components/PostTitle/styles.js
@@ -1,7 +1,30 @@
 import styled from 'styled-components'
+import Image from 'gatsby-image'
 
-export const Title = styled.header`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+export const Container = styled.header`
+  text-align: center;
+  grid-column: 1 / -1;
+  position: relative;
+  color: ${props => props.theme.mainWhite};
+  display: grid;
+  justify-items: center;
+  align-content: center;
+  min-height: 50vh;
+  max-height: 60vh;
+  margin-bottom: calc(3em + 3vh);
+`
+
+export const Img = styled(Image)`
+  position: absolute !important;
+  z-index: -1;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+`
+
+export const Title = styled.h1`
+  background: rgba(0, 0, 0, 0.5);
+  border-radius: ${props => props.theme.mediumBorderRadius};
+  padding: 0.1em 0.5em;
 `
