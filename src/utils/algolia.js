9eea89ecc76eb1ccdb02cd77330878ff6fc98c27
JanoshRiebesell <janosh.riebesell@gmail.com>
2018-10-15 11:40:07 +0100

    feat: add queries for algolia index building in src/utls/algolia

---
+++ src/utils/algolia.js
@@ -0,0 +1,61 @@
+const pageQuery = `{
+  pages: allMarkdownRemark(
+    filter: {
+      fileAbsolutePath: { regex: "/content/pages/" },
+      frontmatter: {purpose: {eq: "page"}}
+    }
+  ) {
+    edges {
+      node {
+        objectID: id
+        frontmatter {
+          title
+          slug
+        }
+        excerpt(pruneLength: 5000)
+      }
+    }
+  }
+}`
+
+const postQuery = `{
+  posts: allMarkdownRemark(
+    filter: { fileAbsolutePath: { regex: "/content/posts/" } }
+  ) {
+    edges {
+      node {
+        objectID: id
+        frontmatter {
+          title
+          slug
+          date(formatString: "MMMM DD, YYYY")
+          categories
+        }
+        excerpt(pruneLength: 5000)
+      }
+    }
+  }
+}`
+
+const queries = [
+  {
+    query: pageQuery,
+    transformer: ({ data }) =>
+      data.pages.edges.map(({ node: { frontmatter, ...rest } }) => ({
+        ...frontmatter,
+        ...rest,
+      })),
+    indexName: `Pages`,
+  },
+  {
+    query: postQuery,
+    transformer: ({ data }) =>
+      data.posts.edges.map(({ node: { frontmatter, ...rest } }) => ({
+        ...frontmatter,
+        ...rest,
+      })),
+    indexName: `Posts`,
+  },
+]
+
+module.exports = queries
