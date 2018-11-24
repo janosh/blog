23c0370b06432cb5b836bfc38c54bedfefca824e
JanoshRiebesell <janosh.riebesell@gmail.com>
2018-11-24 11:55:27 +0000

    style: typography reduce maxFontSize, maxLineHeight

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -2,9 +2,9 @@ const typography = {
   fonts: `-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif`,
   // font sizes and line heigths in em units
   minFontSize: 1,
-  maxFontSize: 1.3,
+  maxFontSize: 1.2,
   minLineHeight: 1.5,
-  maxLineHeight: 2,
+  maxLineHeight: 1.8,
 }
 
 export default typography
