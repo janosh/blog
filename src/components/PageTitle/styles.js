d36ec2f6f1a3976986d1c8b135814ebde5638ef5
JanoshRiebesell <janosh.riebesell@gmail.com>
2019-01-20 13:41:34 +0000

    style: PageTitle

--- src/components/PageTitle/styles.js
+++ src/components/PageTitle/styles.js
@@ -1,25 +1,31 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import Img from 'gatsby-image'
 
+const backdrop = css`
+  background: rgba(0, 0, 0, 0.5);
+  border-radius: ${props => props.theme.mediumBorderRadius};
+  > * {
+    margin: 0.2em;
+    width: max-content;
+  }
+`
+
 export const Title = styled.header`
-  margin-bottom: calc(2em + 2vh);
   grid-column: 2 / -2;
   grid-row: 1;
   text-align: center;
   color: ${props => props.theme.white};
   font-size: calc(1em + 1vw);
   align-self: center;
+  ${props => props.backdrop && backdrop};
 `
 
 export const Background = styled(Img)`
   margin-bottom: calc(2em + 2vh);
   z-index: -1;
   grid-row: 1;
-  height: 60vh;
+  height: 70vh;
   top: 0;
   left: 0;
   grid-column: 1 / -1 !important;
-  img {
-    object-position: bottom !important;
-  }
 `
