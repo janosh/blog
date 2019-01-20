56b3c27fe8147a24babd90d16d97cddf8b28a745
JanoshRiebesell <janosh.riebesell@gmail.com>
2019-01-20 13:41:34 +0000

    style: PageTitle

--- src/components/PageTitle/index.js
+++ src/components/PageTitle/index.js
@@ -3,17 +3,17 @@ import { StaticQuery, graphql } from 'gatsby'
 
 import { Title, Background } from './styles'
 
-const PageTitle = ({ children, file }) => (
+const PageTitle = ({ children, img, defaultImg, backdrop }) => (
   <>
-    <Background fluid={file.img.fluid} />
-    <Title>{children}</Title>
+    <Background fluid={(img && img.fluid) || defaultImg.fluid} />
+    <Title backdrop={backdrop}>{children}</Title>
   </>
 )
 
 const query = graphql`
   {
     file(name: { eq: "background" }) {
-      img: childImageSharp {
+      defaultImg: childImageSharp {
         fluid(maxWidth: 2500, quality: 100) {
           ...GatsbyImageSharpFluid_withWebp
         }
@@ -25,6 +25,6 @@ const query = graphql`
 export default props => (
   <StaticQuery
     query={query}
-    render={data => <PageTitle {...data} {...props} />}
+    render={data => <PageTitle {...data.file} {...props} />}
   />
 )
