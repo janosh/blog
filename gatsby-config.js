e649fe344ba8642b822da5e0b91e4c5176ca4c0d
JanoshRiebesell <janosh.riebesell@gmail.com>
2019-01-20 13:43:33 +0000

    style: remove old Projects styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,7 +58,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GA_TRACKING_ID,
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
       },
     },
     {
