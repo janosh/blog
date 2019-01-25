11835a32edee5c70b45ad74ae22304365a7df4a7
JanoshRiebesell <janosh.riebesell@gmail.com>
2019-01-25 16:57:17 +0000

    refactor: tag template

--- src/templates/tag.js
+++ src/templates/tag.js
@@ -1,35 +1,30 @@
-import React from 'react'
-import { graphql } from 'gatsby'
+import React from "react"
+import { graphql } from "gatsby"
 
-import Global from '../components/Global'
-import PageTitle from '../components/PageTitle'
-import TagList from '../components/TagList'
-import PostList from '../components/PostList'
+import Global from "../components/Global"
+import PageTitle from "../components/PageTitle"
+import PostList from "../views/PostList"
 
 const tagTemplate = ({ data, location, title = `Blog`, pageContext }) => {
   let { posts, tags } = data
-  const path = location.pathname
-  const tag = pageContext.title
-  tags = tags.group.map(({ title, totalCount }) => ({
-    title,
-    slug: `/blog/` + title.toLowerCase().replace(` `, `-`),
-    totalCount,
-  }))
-  tags.unshift({ title: `All`, slug: `/blog`, totalCount: posts.edges.length })
-  if (tag !== `All`) {
+  const tagTitle = pageContext.title
+  const postCount = posts.edges.length
+  if (tagTitle !== `All`) {
     posts.edges = posts.edges.filter(({ node }) =>
-      node.frontmatter.tags.includes(tag)
+      node.frontmatter.tags.includes(tagTitle)
     )
   }
+  tags = tags.group.map(tag => ({
+    ...tag,
+    slug: `blog/` + tag.title.toLowerCase().replace(` `, `-`),
+  }))
+  tags.unshift({ title: `All`, slug: `blog`, count: postCount })
   return (
-    <Global pageTitle={title} path={path}>
+    <Global pageTitle={title} path={location.pathname}>
       <PageTitle>
         <h1>{title}</h1>
       </PageTitle>
-      <div>
-        <TagList tags={tags} />
-        {posts && <PostList posts={posts.edges} />}
-      </div>
+      <PostList posts={posts.edges} tags={tags} />
     </Global>
   )
 }
@@ -51,7 +46,7 @@ export const query = graphql`
     tags: allMarkdownRemark {
       group(field: frontmatter___tags) {
         title: fieldValue
-        totalCount
+        count: totalCount
       }
     }
   }
