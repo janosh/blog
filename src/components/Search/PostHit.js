a8902402030b8f367917e215422d7185a7847d9f
JanoshRiebesell <janosh.riebesell@gmail.com>
2019-01-27 10:34:25 +0000

    fix: after removing tag pages, PostHit tags can no longer be links

--- src/components/Search/PostHit.js
+++ src/components/Search/PostHit.js
@@ -21,7 +21,7 @@ const PostHit = clickHandler => ({ hit }) => (
       {hit.tags.map((tag, index) => (
         <Fragment key={tag}>
           {!!index && `, `}
-          <Link to={`blog/` + tag.toLowerCase().replace(` `, `-`)}>{tag}</Link>
+          {tag}
         </Fragment>
       ))}
     </div>
