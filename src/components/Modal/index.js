3a6945dce6a42bad4ca9e40d5a1b238a33bdd1af
Janosh Riebesell <janosh.riebesell@gmail.com>
2019-02-06 12:16:42 +0100

    feat: Modal add options for navigation (next, prev) and white controls

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,16 +1,38 @@
-import React from 'react'
+import React from "react"
 
-import { ModalBackground, ModalContainer, Close } from './styles'
+import {
+  ModalBackground,
+  ModalContainer,
+  Close,
+  Next,
+  Previous,
+} from "./styles"
 
-const Modal = ({ open, closeModal, children }) => {
-  return (
-    <ModalBackground open={open} onClick={closeModal}>
-      <ModalContainer onClick={event => event.stopPropagation()}>
-        <Close onClick={closeModal} />
-        {children}
-      </ModalContainer>
-    </ModalBackground>
-  )
-}
+const Modal = ({
+  open,
+  modal,
+  setModal,
+  children,
+  navigation,
+  white,
+  className,
+}) => (
+  <ModalBackground open={open} onClick={setModal}>
+    <ModalContainer
+      onClick={event => event.stopPropagation()}
+      className={className}
+    >
+      <Close onClick={setModal} white={white} />
+      {children}
+      {navigation && (
+        <>
+          <Next onClick={() => setModal(modal + 1)} white={white} />
+          <Previous onClick={() => setModal(modal - 1)} white={white} />
+        </>
+      )}
+      {children}
+    </ModalContainer>
+  </ModalBackground>
+)
 
 export default Modal
