498e0730733011bca4b207cc41fc734703e48669
janosh <janosh.riebesell@gmail.com>
2019-02-22 22:20:32 +0000

    feat: Modal listen for left/right arrow keys to decrement/increment modal index

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 
 import {
   ModalBackground,
@@ -16,23 +16,34 @@ const Modal = ({
   navigation,
   white,
   className,
-}) => (
-  <ModalBackground open={open} onClick={setModal}>
-    <ModalContainer
-      onClick={event => event.stopPropagation()}
-      className={className}
-    >
-      <Close onClick={setModal} white={white} />
-      {children}
-      {navigation && (
-        <>
-          <Next onClick={() => setModal(modal + 1)} white={white} />
-          <Previous onClick={() => setModal(modal - 1)} white={white} />
-        </>
-      )}
-      {children}
-    </ModalContainer>
-  </ModalBackground>
-)
+}) => {
+  const handleArrowKeys = event => {
+    if (open && event.key === `ArrowRight`) setModal(modal + 1)
+    else if (open && event.key === `ArrowLeft`) setModal(modal - 1)
+  }
+  useEffect(() => {
+    document.addEventListener(`keydown`, handleArrowKeys)
+    return () => document.removeEventListener(`keydown`, handleArrowKeys)
+  })
+  return (
+    // passing setModal without arguments will close the modal when triggered
+    <ModalBackground open={open} onClick={setModal}>
+      <ModalContainer
+        onClick={event => event.stopPropagation()}
+        className={className}
+      >
+        <Close onClick={setModal} white={white} />
+        {children}
+        {navigation && (
+          <>
+            <Next onClick={() => setModal(modal + 1)} white={white} />
+            <Previous onClick={() => setModal(modal - 1)} white={white} />
+          </>
+        )}
+        {children}
+      </ModalContainer>
+    </ModalBackground>
+  )
+}
 
 export default Modal
