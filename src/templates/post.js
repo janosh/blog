9cc4c2f073eb00091e7831af3a28dae676f78995
janosh <janosh.riebesell@gmail.com>
2019-02-23 20:04:49 +0000

    refactor: post template remove graphql fragment, use new PageHeader comp

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,18 +1,21 @@
-import React from 'react'
-import { graphql } from 'gatsby'
+import React from "react"
+import { graphql } from "gatsby"
 
-import Global from '../components/Global'
-import PostTitle from '../components/PostTitle'
+import Global from "../components/Global"
+import PageHeader from "../components/PageHeader"
+import PostMeta from "../components/PostMeta"
 
 const PostTemplate = ({ data, location }) => {
-  const { frontmatter, excerpt, html, ...rest } = data.post
+  const { frontmatter, excerpt, html, timeToRead } = data.post
+  const { title, date, cover } = frontmatter
+  if (cover) cover.fluid = cover.img.sharp.fluid
+  const meta = { date, timeToRead }
   return (
-    <Global
-      pageTitle={frontmatter.title}
-      path={location.pathname}
-      description={excerpt}
-    >
-      <PostTitle {...frontmatter} {...rest} />
+    <Global pageTitle={title} path={location.pathname} description={excerpt}>
+      <PageHeader img={cover} backdrop>
+        <h1>{title}</h1>
+        <PostMeta inTitle {...meta} />
+      </PageHeader>
       <div dangerouslySetInnerHTML={{ __html: html }} />
     </Global>
   )
@@ -21,31 +24,9 @@ const PostTemplate = ({ data, location }) => {
 export default PostTemplate
 
 export const query = graphql`
-  fragment postFields on MarkdownRemark {
-    frontmatter {
-      title
-      slug
-      date(formatString: "MMMM DD, YYYY")
-      tags
-      cover {
-        img {
-          sharp: childImageSharp {
-            fluid(maxWidth: 2500, quality: 100) {
-              ...GatsbyImageSharpFluid_withWebp
-            }
-          }
-        }
-        credit
-        url
-      }
-    }
-    timeToRead
-    excerpt(pruneLength: 300)
-    html
-  }
   query($slug: String!) {
     post: markdownRemark(frontmatter: { slug: { eq: $slug } }) {
-      ...postFields
+      ...post
     }
   }
 `
