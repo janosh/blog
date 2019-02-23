6c2afcb42b1a56a66737e54f91bbec78d0c58dd1
janosh <janosh.riebesell@gmail.com>
2019-02-23 20:08:53 +0000

    style: PostMeta simplify inTitle css

--- src/components/PostMeta/styles.js
+++ src/components/PostMeta/styles.js
@@ -3,11 +3,8 @@ import styled, { css } from "styled-components"
 import mediaQuery from "../../utils/mediaQuery"
 
 const inTitle = css`
-  justify-items: center;
-  padding: 0.2em 1em;
-  margin: 1.3em 0;
-  background: rgba(0, 0, 0, 0.5);
-  border-radius: ${props => props.theme.mediumBorderRadius};
+  margin: 1.3em auto;
+  width: max-content;
   a {
     color: ${props => props.theme.lightGreen};
   }
@@ -17,7 +14,7 @@ export const Meta = styled.div`
   display: grid;
   grid-auto-columns: max-content;
   grid-gap: calc(0.5em + 1vw);
-  font-size: 0.9em;
+  font-size: 0.8em;
   > span {
     display: flex;
     align-items: center;
