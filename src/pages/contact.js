b51a68428896b387448aca88bd055bf7b7445488
janosh <janosh.riebesell@gmail.com>
2019-02-23 20:08:12 +0000

    feat: contact page add custom cover image

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,33 +1,43 @@
-import React from 'react'
+import React from "react"
+import { graphql } from "gatsby"
 
-import Global from '../components/Global'
-import PageTitle from '../components/PageTitle'
-import Social from '../components/Social'
+import Global from "../components/Global"
+import PageHeader from "../components/PageHeader"
+import Social from "../components/Social"
 
-const ContactPage = ({ location }) => {
-  const path = location.pathname
-  return (
-    <Global path={path}>
-      <PageTitle>
-        <h1>Contact</h1>
-      </PageTitle>
-      <p>You can reach me by email or on any of these platforms.</p>
-      <Social size="2em" />
-      <p>
-        I spend most of my days at the Maxwell Centre. If you‘re in the area,
-        feel free to drop by.
-      </p>
-      <iframe
-        title="contactMap"
-        width="100%"
-        height="600px"
-        frameBorder="0"
-        src={`https://www.google.com/maps/embed/v1/place?key=${
-          process.env.GATSBY_GOOGLE_MAPS_API_KEY
-        }&q=Maxwell+Centre,Cambridge,UK`}
-      />
-    </Global>
-  )
-}
+const ContactPage = ({ data, location }) => (
+  <Global path={location.pathname}>
+    <PageHeader img={data.img.sharp}>
+      <h1>Contact</h1>
+    </PageHeader>
+    <p>You can reach me by email or on any of these platforms.</p>
+    <Social size="2em" />
+    <p>
+      I spend most of my days at the Maxwell Centre. If you‘re in the area, feel
+      free to drop by.
+    </p>
+    <iframe
+      title="contactMap"
+      width="100%"
+      height="600px"
+      frameBorder="0"
+      src={`https://www.google.com/maps/embed/v1/place?key=${
+        process.env.GATSBY_GOOGLE_MAPS_API_KEY
+      }&q=Maxwell+Centre,Cambridge,UK`}
+    />
+  </Global>
+)
 
 export default ContactPage
+
+export const query = graphql`
+  {
+    img: file(name: { eq: "mt-meru" }) {
+      sharp: childImageSharp {
+        fluid(quality: 100, maxWidth: 2000) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
