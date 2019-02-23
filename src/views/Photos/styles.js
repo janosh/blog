ed4fb606f0df80dba719516638530f76204b902c
janosh <janosh.riebesell@gmail.com>
2019-02-23 20:00:20 +0000

    feat: Photos views add captions

--- src/views/Photos/styles.js
+++ src/views/Photos/styles.js
@@ -1,8 +1,13 @@
-import styled, { css } from "styled-components"
+import styled from "styled-components"
 import Img from "gatsby-image"
 
 export const Thumbnail = styled(Img)`
   border-radius: ${props => props.theme.mediumBorderRadius};
+  transition: ${props => props.theme.shortTrans};
+  height: 100%;
+  :hover {
+    transform: scale(1.05);
+  }
 `
 
 export const LargeImg = styled(Img)`
@@ -10,7 +15,14 @@ export const LargeImg = styled(Img)`
   height: 80vh;
 `
 
-export const modalCss = css`
-  padding: 0;
-  max-width: 80vw;
+export const Caption = styled.h3`
+  position: absolute;
+  bottom: 0;
+  right: 1.3em;
+  margin: 0;
+  padding: 0.1em 0.5em;
+  color: white;
+  border-radius: ${props => (props.theme.mediumBorderRadius + ` `).repeat(2)} 0
+    0;
+  background: rgba(0, 0, 0, 0.7);
 `
