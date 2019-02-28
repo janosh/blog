a0c68637fcf9624633b021d8997beb94b61027c4
janosh <janosh.riebesell@gmail.com>
2019-02-28 11:55:11 +0000

    chore: remove unneeded grid-column: 2/-2; from posts and photos views

--- src/views/PostList.js
+++ src/views/PostList.js
@@ -4,10 +4,8 @@ import PropTypes from "prop-types"
 import Grid from "../components/styles/Grid"
 import PostExcerpt from "../components/PostExcerpt"
 
-const wideLayout = `grid-column: 2 / -2;`
-
 const PostList = ({ posts }) => (
-  <Grid minWidth="17em" maxWidth="24em" gap="1.5em" css={wideLayout}>
+  <Grid minWidth="17em" maxWidth="24em" gap="1.5em">
     {posts.map(({ node }) => (
       <PostExcerpt key={node.frontmatter.slug} post={node} />
     ))}
