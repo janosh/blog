bb2e0d30d477b2a65dba07a5109a52fb4d85e6ed
janosh <janosh.riebesell@gmail.com>
2019-02-28 11:48:14 +0000

    refactor: Footer useStaticQuery

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,17 +1,20 @@
-import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
-import PropTypes from 'prop-types'
+import React from "react"
 
-import { FooterContainer, Copyright, Source, PoweredBy } from './styles'
+import { FooterContainer, PoweredBy } from "./styles"
+import query from "./query"
 
-const Footer = ({ footer, logos }) => {
+const Footer = () => {
+  const { footer, logos } = query()
   const { copyright, sourceNote, poweredBy } = footer
   return (
     <FooterContainer>
-      <Copyright>
+      <span css="grid-area: copyright;">
         © {new Date().getFullYear()} - {copyright}
-      </Copyright>
-      <Source dangerouslySetInnerHTML={{ __html: sourceNote }} />
+      </span>
+      <span
+        css="grid-area: source;"
+        dangerouslySetInnerHTML={{ __html: sourceNote }}
+      />
       <PoweredBy>
         Powered by
         {poweredBy.map(({ url, title }, index) => (
@@ -24,34 +27,4 @@ const Footer = ({ footer, logos }) => {
   )
 }
 
-Footer.propTypes = {
-  footer: PropTypes.object.isRequired,
-  logos: PropTypes.object.isRequired,
-}
-
-const query = graphql`
-  {
-    footer: footerYaml {
-      copyright
-      sourceNote
-      poweredBy {
-        url
-        title
-      }
-    }
-    logos: allFile(
-      filter: { dir: { regex: "/footer/logos/" } }
-      sort: { fields: name }
-    ) {
-      edges {
-        node {
-          src: publicURL
-        }
-      }
-    }
-  }
-`
-
-export default props => (
-  <StaticQuery query={query} render={data => <Footer {...data} {...props} />} />
-)
+export default Footer
