282a13d5c8ebf8f33985164df6f3450699d0aea5
janosh <janosh.riebesell@gmail.com>
2019-02-28 11:49:23 +0000

    refactor: simplify Header styles

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,31 +1,20 @@
-import styled, { css } from 'styled-components'
-import { Link } from 'gatsby'
+import styled from "styled-components"
+import { Link } from "gatsby"
 
-import mediaQuery from '../../utils/mediaQuery'
+import mediaQuery from "../../utils/mediaQuery"
 
-const transparent = css`
-  position: absolute;
-  background: rgba(0, 0, 0, 0.5);
-  width: fill-available;
-  z-index: 3;
-`
-
-const opaque = css`
+export const HeaderContainer = styled.header`
   background: ${props => props.theme.darkBlue};
-`
-
-export const Container = styled.div`
-  ${props => (props.transparent ? transparent : opaque)};
   display: grid;
   grid-gap: 3vw;
   justify-items: center;
   justify-content: space-between;
-  grid-template-areas: 'nav title social search';
+  grid-template-areas: "nav title social search";
   grid-template-columns: auto 1fr auto auto;
   padding: 2vmin 3vmin;
   ${mediaQuery.minPhablet} {
     justify-items: start;
-    grid-template-areas: 'title nav social search';
+    grid-template-areas: "title nav social search";
   }
 `
 
