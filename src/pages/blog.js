8ce3e8678024f3161d1c491d8f09044dc9cb124e
janosh <janosh.riebesell@gmail.com>
2019-02-28 11:51:03 +0000

    feat: blog read and write active tag from/to url param

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,27 +1,36 @@
 import React, { useState } from "react"
-import { graphql } from "gatsby"
+import { graphql, navigate } from "gatsby"
 
 import Global from "../components/Global"
-import PageHeader from "../components/PageHeader"
+import PageTitle from "../components/PageTitle"
+import PageBody from "../components/styles/PageBody"
 import TagList from "../components/TagList"
 import PostList from "../views/PostList"
+import { paramCase, titleCase } from "../utils/case"
 
-const BlogPage = ({ data, location, title = `Blog` }) => {
+const BlogPage = ({ data, location }) => {
   const { posts, tags, img } = data
-  const [tag, setTag] = useState(`All`)
+  const urlTag = titleCase(location.search.replace(/.*tag=([^&]+).*/, `$1`))
+  const [tag, setTag] = useState(urlTag || `All`)
   const filteredPosts =
     tag === `All`
       ? posts.edges
       : posts.edges.filter(({ node }) => node.frontmatter.tags.includes(tag))
   if (!tags.group.map(tag => tag.title).includes(`All`))
     tags.group.unshift({ title: `All`, count: posts.edges.length })
+  const handleTagClick = tag => {
+    setTag(tag)
+    navigate(tag === `All` ? `/blog` : `/blog?tag=${paramCase(tag)}`)
+  }
   return (
-    <Global pageTitle={title} path={location.pathname}>
-      <PageHeader img={img && img.sharp}>
-        <h1>{title}</h1>
-      </PageHeader>
-      <TagList tags={tags.group} activeTag={tag} setTag={setTag} />
-      <PostList posts={filteredPosts} />
+    <Global pageTitle="Blog" path={location.pathname}>
+      <PageTitle img={img && img.sharp}>
+        <h1>Blog</h1>
+      </PageTitle>
+      <PageBody cols="2/-2">
+        <TagList tags={tags.group} activeTag={tag} setTag={handleTagClick} />
+        <PostList posts={filteredPosts} />
+      </PageBody>
     </Global>
   )
 }
