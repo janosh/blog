7f58a17064ac552772a39c151a48d5456e5bfc8d
janosh <janosh.riebesell@gmail.com>
2019-02-28 12:29:59 +0000

    fix: LandingTitle position

--- src/components/LandingTitle/styles.js
+++ src/components/LandingTitle/styles.js
@@ -1,9 +1,12 @@
-import styled from 'styled-components'
-import Img from 'gatsby-image'
-import mediaQuery from '../../utils/mediaQuery'
+import styled from "styled-components"
+import Img from "gatsby-image"
+import mediaQuery from "../../utils/mediaQuery"
 
 export const Title = styled.header`
-  grid-area: 2 / 2 / 2 / -2;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   justify-self: center;
   justify-items: center;
   padding: calc(0.5em + 2vh) calc(0.5em + 2vw);
@@ -14,13 +17,13 @@ export const Title = styled.header`
   display: grid;
   grid-gap: 1em;
   grid-template-areas:
-    'me'
-    'hello'
-    'about';
+    "me"
+    "hello"
+    "about";
   ${mediaQuery.minPhone} {
     grid-template-areas:
-      'me hello'
-      'about about';
+      "me hello"
+      "about about";
     grid-template-columns: min-content;
   }
   a {
