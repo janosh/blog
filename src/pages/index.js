cf83cb48d75034f7fadc4b2076456f570c71d2fc
janosh <janosh.riebesell@gmail.com>
2019-03-01 20:16:49 +0000

    feat: redo landing page, add recent posts and projects

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,42 +3,94 @@ import { graphql } from 'gatsby'
 import Img from 'gatsby-image'
 
 import Global from '../components/Global'
-import Slideshow from '../components/Slideshow'
-import LandingTitle from '../components/LandingTitle'
+import PageTitle from '../components/PageTitle'
+import Scroll from '../components/Scroll'
+import PageBody from '../components/styles/PageBody'
+import PostList from '../views/PostList'
+import Projects from '../views/Projects'
 
-const IndexPage = ({ data: { photos, me }, location }) => (
-  <Global margin="0" transparent path={location.pathname}>
-    <Slideshow>
-      {photos.edges.map(({ node }) => (
-        <Img key={node.name} fluid={node.img.fluid} alt={node.name} />
-      ))}
-    </Slideshow>
-    <LandingTitle me={me} />
-  </Global>
-)
+const IndexPage = ({ data, location }) => {
+  const { md, janosh, posts, projects } = data
+  const img = {
+    ...md.frontmatter.cover,
+    fluid: md.frontmatter.cover.img.sharp.fluid,
+  }
+  return (
+    <Global margin="0" transparent path={location.pathname}>
+      <PageTitle img={img} fillToBottom>
+        <h1 css="border: 1px solid white; padding: 1em; background: rgba(0,0,0,0.3);">
+          {md.frontmatter.title}
+        </h1>
+        <Scroll direction="down" to={1} align="center" position="absolute" />
+      </PageTitle>
+      <PageBody>
+        <Img
+          fixed={janosh.img.fixed}
+          css="border-radius: 50%; justify-self: center;"
+        />
+        <p dangerouslySetInnerHTML={{ __html: md.html }} />
+        <h1 css="justify-self: center;">Recent posts</h1>
+        <PostList
+          posts={posts.edges}
+          css="grid-column: 2/-2; grid-auto-flow: column; overflow: scroll;"
+        />
+        <h1 css="justify-self: center;">Recent projects</h1>
+        <Projects
+          {...projects}
+          css="grid-column: 2/-2; grid-auto-flow: column; overflow: scroll;"
+        />
+      </PageBody>
+    </Global>
+  )
+}
 
 export default IndexPage
 
 export const query = graphql`
   {
-    photos: allFile(
-      filter: { relativeDirectory: { eq: "pages/index/photos" } }
-    ) {
-      edges {
-        node {
-          name
-          img: childImageSharp {
-            fluid(quality: 100, maxWidth: 2500) {
-              ...GatsbyImageSharpFluid_withWebp
-            }
-          }
+    md: markdownRemark(fileAbsolutePath: { regex: "/landing.md/" }) {
+      frontmatter {
+        title
+        ...cover
+      }
+      html
+    }
+    janosh: file(name: { eq: "janosh" }) {
+      img: childImageSharp {
+        fixed(width: 175) {
+          ...GatsbyImageSharpFixed_withWebp
         }
       }
     }
-    me: file(name: { eq: "me" }) {
-      img: childImageSharp {
-        fluid(maxWidth: 500) {
-          ...GatsbyImageSharpFluid_withWebp
+    posts: allMarkdownRemark(
+      filter: { fileAbsolutePath: { regex: "/posts/" } }
+      sort: { fields: frontmatter___date, order: DESC }
+      limit: 5
+    ) {
+      edges {
+        node {
+          ...post
+        }
+      }
+    }
+    projects: allMarkdownRemark(
+      filter: { fileAbsolutePath: { regex: "/pages/web/projects/" } }
+      sort: { fields: frontmatter___date, order: DESC }
+      limit: 5
+    ) {
+      projects: edges {
+        node {
+          html
+          frontmatter {
+            title
+            slug
+            date(formatString: "MMM DD, YYYY")
+            url
+            repo
+            npm
+            tech
+            ...cover
+          }
         }
       }
     }
