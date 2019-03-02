f8d20cf4c87cee2cfea8b198edc06897f6bbb487
janosh <janosh.riebesell@gmail.com>
2019-03-02 21:58:27 +0000

    feat: improve PostList view layout on wide screens

--- src/views/PostList/index.js
+++ src/views/PostList/index.js
@@ -1,15 +1,16 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-import Grid from "../components/styles/Grid"
-import PostExcerpt from "../components/PostExcerpt"
+import PostExcerpt from "../../components/PostExcerpt"
 
-const PostList = ({ posts, className }) => (
-  <Grid minWidth="17em" maxWidth="24em" gap="1.5em" className={className}>
+import { PostGrid } from "./styles"
+
+const PostList = ({ posts }) => (
+  <PostGrid minWidth="17em" maxWidth="24em" gap="1.5em">
     {posts.map(({ node }) => (
       <PostExcerpt key={node.frontmatter.slug} post={node} />
     ))}
-  </Grid>
+  </PostGrid>
 )
 
 export default PostList
