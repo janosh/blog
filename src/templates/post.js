9f758c740bceafec23484042bdc98ca073938f8e
janosh <janosh.riebesell@gmail.com>
2019-03-02 18:17:54 +0000

    feat: post template link next and previous posts

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,23 +1,44 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Global from "../components/Global"
 import PageTitle from "../components/PageTitle"
 import PageBody from "../components/styles/PageBody"
 import PostMeta from "../components/PostMeta"
 
-const PostTemplate = ({ data, location }) => {
+const PostTemplate = ({ data, location, pageContext }) => {
   const { frontmatter, excerpt, html, timeToRead } = data.post
   const { title, date, cover } = frontmatter
   if (cover) cover.fluid = cover.img.sharp.fluid
   const meta = { date, timeToRead }
+  const { next, previous } = pageContext
   return (
     <Global pageTitle={title} path={location.pathname} description={excerpt}>
       <PageTitle img={cover} backdrop>
         <h1>{title}</h1>
         <PostMeta inTitle {...meta} />
       </PageTitle>
-      <PageBody dangerouslySetInnerHTML={{ __html: html }} />
+      <PageBody>
+        <div dangerouslySetInnerHTML={{ __html: html }} />
+        <div css="display: flex; justify-content: space-between; flex-wrap: wrap; margin-top: 2em;">
+          {previous && (
+            <div css="margin-bottom: 1em;">
+              <h4 css="margin: 0;">Previous post</h4>
+              <Link to={`/blog` + previous.frontmatter.slug} rel="prev">
+                ← {previous.frontmatter.title}
+              </Link>
+            </div>
+          )}
+          {next && (
+            <div css="margin-left: auto; padding-left: 1em; text-align: right;">
+              <h4 css="margin: 0;">Next post</h4>
+              <Link to={`/blog` + next.frontmatter.slug} rel="next">
+                {next.frontmatter.title} →
+              </Link>
+            </div>
+          )}
+        </div>
+      </PageBody>
     </Global>
   )
 }
