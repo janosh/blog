a6a64c86f905dacf873db2c762c9a8ff9f99bcd9
janosh <janosh.riebesell@gmail.com>
2019-03-02 17:58:09 +0000

    feat: add projects fragment

---
+++ src/utils/fragments/projects.js
@@ -0,0 +1,26 @@
+import { graphql } from "gatsby"
+
+export const query = graphql`
+  fragment projects on Query {
+    projects: allMarkdownRemark(
+      filter: { fileAbsolutePath: { regex: "/pages/web/projects/" } }
+      sort: { fields: frontmatter___date, order: DESC }
+    ) {
+      projects: edges {
+        node {
+          html
+          frontmatter {
+            title
+            slug
+            date(formatString: "MMM DD, YYYY")
+            url
+            repo
+            npm
+            tech
+            ...cover
+          }
+        }
+      }
+    }
+  }
+`
