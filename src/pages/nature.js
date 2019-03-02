76669639f84d10e2c1eac383bb63b8a662f87d08
janosh <janosh.riebesell@gmail.com>
2019-03-02 18:00:32 +0000

    content: rename photography page to nature

--- src/pages/nature.js
+++ src/pages/nature.js
@@ -6,25 +6,22 @@ import PageTitle from "../components/PageTitle"
 import PageBody from "../components/styles/PageBody"
 import Photos from "../views/Photos"
 
-const PhotographyPage = ({ data, location }) => {
+const NaturePage = ({ data, location }) => {
   const { photos } = data
   const photo = photos.edges[Math.round(Math.random() * photos.edges.length)]
   return (
     <Global path={location.pathname}>
       <PageTitle img={photo.node.img.sharp}>
-        <h1>Photography</h1>
+        <h1>Nature</h1>
       </PageTitle>
       <PageBody cols="2/-2">
         <Photos photos={photos.edges} />
-        <p css="text-align: right;">
-          These images are for personal enjoyment only. All rights reserved.
-        </p>
       </PageBody>
     </Global>
   )
 }
 
-export default PhotographyPage
+export default NaturePage
 
 export const query = graphql`
   {
