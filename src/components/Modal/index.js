5b217111d8e2ff04ce245680d08f4c104b7b570f
janosh <janosh.riebesell@gmail.com>
2019-03-03 11:38:14 +0000

    feat: improve Modal performance by only rendering and adding event listeners if open

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,7 +1,12 @@
 import React, { useEffect } from "react"
 import PropTypes from "prop-types"
 
-import { ModalBehind, ModalDiv, Close, Next, Previous } from './styles'
+import { ModalBehind, ModalDiv, Close, Next, Prev } from "./styles"
+
+const handleArrowKeys = (modal, setModal) => event => {
+  if (event.key === `ArrowRight`) setModal(modal + 1)
+  else if (event.key === `ArrowLeft`) setModal(modal - 1)
+}
 
 const Modal = ({
   open,
@@ -9,35 +14,34 @@ const Modal = ({
   setModal,
   children,
   navigation,
-  white,
   className,
+  white,
 }) => {
-  const handleArrowKeys = event => {
-    if (open && event.key === `ArrowRight`) setModal(modal + 1)
-    else if (open && event.key === `ArrowLeft`) setModal(modal - 1)
-  }
-  useEffect(() => {
-    document.addEventListener(`keydown`, handleArrowKeys)
-    return () => document.removeEventListener(`keydown`, handleArrowKeys)
-  })
-  return (
-    // passing setModal without arguments will close the modal when triggered
-    <ModalBehind open={open} onClick={setModal}>
-      <ModalDiv
-        onClick={event => event.stopPropagation()}
-        className={className}
-      >
-        <Close onClick={setModal} white={white} />
-        {navigation && (
-          <>
-            <Next onClick={() => setModal(modal + 1)} white={white} />
-            <Previous onClick={() => setModal(modal - 1)} white={white} />
-          </>
-        )}
-        {children}
-      </ModalDiv>
-    </ModalBehind>
-  )
+  if (open) {
+    useEffect(() => {
+      const handler = handleArrowKeys(modal, setModal)
+      document.addEventListener(`keydown`, handler)
+      return () => document.removeEventListener(`keydown`, handler)
+    })
+    return (
+      // passing setModal without arguments will close the modal when triggered
+      <ModalBehind open={open} onClick={setModal}>
+        <ModalDiv
+          onClick={event => event.stopPropagation()}
+          className={className}
+        >
+          <Close onClick={setModal} white={white} />
+          {navigation && (
+            <>
+              <Next onClick={() => setModal(modal + 1)} white={white} />
+              <Prev onClick={() => setModal(modal - 1)} white={white} />
+            </>
+          )}
+          {children}
+        </ModalDiv>
+      </ModalBehind>
+    )
+  } else return null
 }
 
 export default Modal
