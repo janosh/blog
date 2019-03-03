5e63d90cefc649df7beb81819ae5ad52e0b32456
janosh <janosh.riebesell@gmail.com>
2019-03-03 18:47:18 +0000

    fix: PostList position on IndexPage

--- src/views/PostList/index.js
+++ src/views/PostList/index.js
@@ -5,8 +5,8 @@ import PostExcerpt from "../../components/PostExcerpt"
 
 import { PostGrid } from "./styles"
 
-const PostList = ({ posts }) => (
-  <PostGrid minWidth="17em" maxWidth="24em" gap="1.5em">
+const PostList = ({ posts, ...rest }) => (
+  <PostGrid minWidth="17em" maxWidth="24em" gap="1.5em" {...rest}>
     {posts.map(({ node }) => (
       <PostExcerpt key={node.frontmatter.slug} post={node} />
     ))}
