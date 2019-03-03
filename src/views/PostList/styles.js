73ccd68bb837b05effbced52460ad52d4fe19c3e
janosh <janosh.riebesell@gmail.com>
2019-03-03 18:47:18 +0000

    fix: PostList position on IndexPage

--- src/views/PostList/styles.js
+++ src/views/PostList/styles.js
@@ -1,10 +1,14 @@
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 
 import Grid from "../../components/styles/Grid"
 import mediaQuery from "../../utils/mediaQuery"
 
-export const PostGrid = styled(Grid)`
-  height: max-content;
+const asRow = css`
+  grid-column: 2/-2;
+  grid-auto-flow: column;
+  overflow: scroll;
+`
+const inBlog = css`
   ${mediaQuery.maxPhablet} {
     grid-column: 3;
     justify-self: center;
@@ -13,4 +17,10 @@ export const PostGrid = styled(Grid)`
     grid-row: 1;
     grid-column: 2/-3;
   }
+`
+
+export const PostGrid = styled(Grid)`
+  height: max-content;
+  ${props => props.asRow && asRow};
+  ${props => props.inBlog && inBlog};
 `
