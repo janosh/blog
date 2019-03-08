e2aa148b24cc61d7bfe68f4ff1bc34d7fe293c56
janosh <janosh.riebesell@gmail.com>
2019-03-08 21:52:59 +0000

    refactor: simplify and compress PostExcerpt markup

--- src/components/PostExcerpt/styles.js
+++ src/components/PostExcerpt/styles.js
@@ -5,21 +5,16 @@ export const Post = styled.article`
   height: 100%;
   width: 100%;
   display: grid;
-  background: ${props => props.theme.veryLightGray};
   border-radius: ${props => props.theme.mediumBorderRadius};
   border: 1px solid ${props => props.theme.lightGray};
+  box-shadow: 0 0 1em ${props => props.theme.lightGray};
   overflow: hidden;
-  > main {
-    display: grid;
-    grid-gap: 0.5em;
-    padding: 0.7em 1em;
-    h3,
-    p {
-      margin: 0;
-    }
+  > :not(:first-child) {
+    margin-left: 20px;
+    margin-right: 20px;
   }
-  a:first-child {
-    height: min-content;
+  > :last-child {
+    margin-bottom: 0.5em;
   }
 `
 
