2c828a3b08e3b2ce23cd364bb8ecb56fbeaaa379
janosh <janosh.riebesell@gmail.com>
2019-03-08 22:33:01 +0000

    feat: add tags to PostMeta

--- src/components/PostMeta/index.js
+++ src/components/PostMeta/index.js
@@ -3,18 +3,23 @@ import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import { CommentCount } from "disqus-react"
 
-import { Meta } from "./styles"
+import { Meta, TagList } from "./styles"
 import { Calendar } from "styled-icons/octicons/Calendar"
 import { Timer } from "styled-icons/material/Timer"
 import { Comments } from "styled-icons/fa-solid/Comments"
 import { disqusConfig } from "../../utils/misc"
 
-const PostMeta = ({ title, slug, date, timeToRead, inTitle = false }) => (
+const PostMeta = ({ title, slug, date, timeToRead, tags, inTitle = false }) => (
   <Meta inTitle={inTitle}>
     <span>
       <Calendar size="1.2em" />
       &ensp;
       {date}
+    </span>
+    <span>
+      <Timer size="1.2em" />
+      &ensp;
+      {timeToRead} min read
     </span>
     <span>
       <Comments size="1.2em" />
@@ -23,11 +28,7 @@ const PostMeta = ({ title, slug, date, timeToRead, inTitle = false }) => (
         <CommentCount {...disqusConfig({ slug, title })} />
       </Link>
     </span>
-    <span>
-      <Timer size="1.2em" />
-      &ensp;
-      {timeToRead} min read
-    </span>
+    <TagList tags={tags} />
   </Meta>
 )
 
