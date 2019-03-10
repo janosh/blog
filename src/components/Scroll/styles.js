f763ff9b50a4bed8691ad9543256a93670be2020
janosh <janosh.riebesell@gmail.com>
2019-03-10 23:51:28 +0000

    refactor: rewrite Scroll comp using hooks

--- src/components/Scroll/styles.js
+++ src/components/Scroll/styles.js
@@ -2,34 +2,23 @@ import styled from "styled-components"
 import { ArrowDownCircle as Down } from "styled-icons/feather/ArrowDownCircle"
 import { ArrowUpCircle as Up } from "styled-icons/feather/ArrowUpCircle"
 
-const align = props => {
-  switch (props.align) {
-    case `left`:
-      return `left: 1em;`
-    case `right`:
-      return `right: 1em;`
-    case `center`:
-    default:
-      return `left: calc(50vw - ${props.size} / 2);`
-  }
-}
-
 export const Arrow = styled(Down).attrs(({ direction, size }) => ({
-  as: { up: Up }[direction],
+  as: direction === `up` && Up,
   size,
 }))`
+  ${({ theme, show, size }) => `
   z-index: 2;
-  background: ${props => props.theme.lightGreen};
-  color: ${props => props.theme.white};
+  background: ${theme.lightGreen};
+  color: ${theme.white};
   border-radius: 50%;
-  transition: ${props => props.theme.shortTrans};
-  position: ${props => props.position};
-  bottom: 1em;
-  ${props => align(props)};
-  opacity: ${props => (props.show ? 1 : 0)};
-  visibility: ${props => (props.show ? `visible` : `hidden`)};
+  transition: ${theme.shortTrans};
+  position: absolute;
+  bottom: 0.5em;
+  opacity: ${show ? 1 : 0};
+  visibility: ${show ? `visible` : `hidden`};
   :hover {
     transform: scale(1.15);
-    background: ${props => props.theme.orange};
+    background: ${theme.orange};
   }
+  right: calc(50vw - ${size} / 2);`}
 `
