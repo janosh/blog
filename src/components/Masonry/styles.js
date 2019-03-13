9b072b16254ab86ea26c2fbc55a78283a263b91b
janosh <janosh.riebesell@gmail.com>
2019-03-13 22:50:27 +0000

    feat: add Masonry comp using React hooks

---
+++ src/components/Masonry/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+export const MasonryDiv = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  grid-gap: ${props => props.gap || `1em`};
+`
+
+export const Col = styled.div`
+  display: grid;
+  grid-gap: ${props => props.gap || `1em`};
+  grid-auto-rows: max-content;
+`
