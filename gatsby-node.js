b25b9d0864ae0afd8cba966ba8e5824d2d711cde
janosh <janosh.riebesell@gmail.com>
2019-03-22 23:04:14 +0000

    feat: gatsby-node read photo EXIF and IPTC and createNodeFields for them in onCreateNode

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,7 @@
 const path = require(`path`)
+const fs = require(`fs`)
+const fastExif = require(`fast-exif`)
+const iptc = require(`node-iptc`)
 
 const pageTemplate = path.resolve(`./src/templates/page.js`)
 const postTemplate = path.resolve(`./src/templates/post.js`)
@@ -58,4 +61,34 @@ exports.createPages = async ({ graphql, actions: { createPage } }) => {
       context: { slug, nextSlug, prevSlug },
     })
   })
+}
+
+const latLngReducer = (acc, val, index) => acc + val / Math.pow(60, index)
+
+exports.onCreateNode = async ({ node, actions }) => {
+  if (node.dir && node.dir.includes(`content/photos`) && node.ext === `.jpg`) {
+    await fs.readFile(node.absolutePath, (err, img) => {
+      if (err) throw err
+      actions.createNodeField({
+        node,
+        name: `iptc`,
+        value: iptc(img),
+      })
+    })
+    fastExif
+      .read(node.absolutePath)
+      .then(exifData => {
+        if (exifData && exifData.gps) {
+          const lat = exifData.gps.GPSLatitude.reduce(latLngReducer, 0)
+          const lng = exifData.gps.GPSLongitude.reduce(latLngReducer, 0)
+          actions.createNodeField({
+            node,
+            name: `gps`,
+            value: { lat, lng },
+          })
+        }
+      })
+      // eslint-disable-next-line no-console
+      .catch(console.error)
+  }
 }
