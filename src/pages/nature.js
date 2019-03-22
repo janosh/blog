da7832acb77f272731db0e502f17d4ebf2752279
janosh <janosh.riebesell@gmail.com>
2019-03-22 23:05:52 +0000

    feat: NaturePage query for iptc and exif photo data, add button group to swtich between masonry/map view

--- src/pages/nature.js
+++ src/pages/nature.js
@@ -1,21 +1,35 @@
-import React from "react"
+import React, { useState } from "react"
 import { graphql } from "gatsby"
 
 import Global from "../components/Global"
 import PageTitle from "../components/PageTitle"
 import PageBody from "../components/styles/PageBody"
+import ButtonGroup from "../components/styles/ButtonGroup"
 import Photos from "../views/Photos"
 
 const NaturePage = ({ data, location }) => {
-  const { photos } = data
-  const photo = photos.edges[Math.floor(Math.random() * photos.edges.length)]
+  const [modal, setModal] = useState()
+  const [tab, setTab] = useState(`list`)
+  const photos = data.photos.edges.map(({ node }) => ({
+    ...node.fields,
+    ...node.img,
+  }))
+  const photoProps = { tab, modal, setModal, photos }
+  const buttonProps = tabName => ({
+    className: tab === tabName ? `active` : null,
+    onClick: () => setTab(tabName),
+  })
   return (
     <Global path={location.pathname}>
-      <PageTitle img={photo.node.img.sharp}>
+      <PageTitle img={photos[13].fluid}>
         <h1>Nature</h1>
       </PageTitle>
       <PageBody cols="2/-2">
-        <Photos photos={photos.edges} />
+        <ButtonGroup css="margin-top: 0;">
+          <button {...buttonProps(`list`)}>List</button>
+          <button {...buttonProps(`map`)}>Map</button>
+        </ButtonGroup>
+        <Photos {...photoProps} />
       </PageBody>
     </Global>
   )
@@ -25,15 +39,24 @@ export default NaturePage
 
 export const query = graphql`
   {
-    photos: allPhotosYaml {
+    photos: allFile(
+      filter: { dir: { regex: "/content/photos/" }, ext: { eq: ".jpg" } }
+    ) {
       edges {
         node {
-          title
-          img {
-            sharp: childImageSharp {
-              fluid(maxWidth: 1800) {
-                ...GatsbyImageSharpFluid_withWebp
-              }
+          fields {
+            gps {
+              lat
+              lng
+            }
+            iptc {
+              copyright_notice
+              headline
+            }
+          }
+          img: childImageSharp {
+            fluid(maxWidth: 1800) {
+              ...GatsbyImageSharpFluid_withWebp
             }
           }
         }
