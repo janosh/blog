f8ecdc0684c7bbd03bf0553966a21d5b2d1875df
janosh <janosh.riebesell@gmail.com>
2019-03-22 21:59:54 +0000

    feat: photo-renaming shell script use exif data

--- content/pages/photography/rename.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# make all filenames in directory lowercase and replace spaces with dashes
-
-# file selector, e.g. some/path/* for all files in some/path/
-selector=$1
-
-for file in "$selector";
-  do mv "$file" `echo $file | tr 'A-Z' 'a-z' | tr ' ' '-'`;
-done
