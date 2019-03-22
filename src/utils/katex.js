cea33eae3e9f150a7e0a7f7a2fb0cadad1dde15a
janosh <janosh.riebesell@gmail.com>
2019-03-22 23:12:32 +0000

    feat: katex add custom LaTeX macros

---
+++ src/utils/katex.js
@@ -0,0 +1,10 @@
+const macros = {
+  "\\expec": `\\langle #1\\rangle`,
+  "\\dif": `\\mathrm d`,
+  "\\argmin": `\\operatorname{argmin}`,
+  "\\argmax": `\\operatorname{argmax}`,
+  "\\vec": `{\\boldsymbol{#1}}`,
+  "\\mat": `{\\boldsymbol{#1}}`,
+}
+
+module.exports = macros
