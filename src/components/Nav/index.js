e78c9dcdf8324ba6566a250a9228b4586adbf2b4
janosh <janosh.riebesell@gmail.com>
2019-04-09 22:24:01 +0100

    refactor: Nav use useMediaQuery hook

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,17 +2,18 @@ import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 import PropTypes from "prop-types"
 
+import { useMediaQuery } from "../../utils/mediaQuery"
+
 import MobileNav from "./Mobile"
 import DesktopNav from "./Desktop"
-
 export { navLinkStyle, NavLink } from "./styles"
 
-const Nav = props => (
-  <>
+const Nav = props =>
+  useMediaQuery(`maxPhablet`) ? (
     <MobileNav {...props} />
+  ) : (
     <DesktopNav {...props} />
-  </>
-)
+  )
 
 const query = graphql`
   {
