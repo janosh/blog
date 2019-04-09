2be0cb20f39f13dfcc9d225f401758fbf618decf
janosh <janosh.riebesell@gmail.com>
2019-04-09 22:22:17 +0100

    feat: add useMediaQuery + refactor mediaQuery obj

--- src/utils/mediaQuery.js
+++ src/utils/mediaQuery.js
@@ -1,30 +1,46 @@
-const min = width => `@media only screen and (min-width: ${width}em)`
-const max = width => `@media only screen and (max-width: ${width}em)`
+import { useState, useEffect } from "react"
+import { titleCase } from "."
 
-export const screenSize = {
-  // screen sizes in em units
-  phone: 30,
-  phablet: 40,
-  tablet: 50,
-  netbook: 60,
-  laptop: 70,
-  desktop: 100,
-}
+const min = width => `only screen and (min-width: ${width}em)`
+const max = width => `only screen and (max-width: ${width}em)`
 
 const mediaQuery = {
-  maxPhone: max(screenSize.phone),
-  maxPhablet: max(screenSize.phablet),
-  maxTablet: max(screenSize.tablet),
-  maxNetbook: max(screenSize.netbook),
-  maxLaptop: max(screenSize.laptop),
-  maxDesktop: max(screenSize.desktop),
+  screens: {
+    // screen sizes in em units
+    phone: 30,
+    phablet: 40,
+    tablet: 50,
+    netbook: 60,
+    laptop: 70,
+    desktop: 100,
+  },
+}
 
-  minPhone: min(screenSize.phone),
-  minPhablet: min(screenSize.phablet),
-  minTablet: min(screenSize.tablet),
-  minNetbook: min(screenSize.netbook),
-  minLaptop: min(screenSize.laptop),
-  minDesktop: min(screenSize.desktop),
+for (const key of Object.keys(mediaQuery.screens)) {
+  const Key = titleCase(key)
+  for (const [func, name] of [[min, `min`], [max, `max`]]) {
+    // css query
+    const query = func(mediaQuery.screens[key])
+    mediaQuery[name + Key] = `@media ` + query
+    // js query (see window.matchMedia)
+    mediaQuery[name + Key + `Js`] = query
+  }
 }
 
 export default mediaQuery
+
+// React hook for JS media queries
+export const useMediaQuery = cond => {
+  if (typeof window !== `undefined`) {
+    if (!mediaQuery[cond + `Js`])
+      throw `useMediaQuery's condition should be one of (min|max)(Phone|Phablet|Tablet|etc.)`
+    const query = window.matchMedia(mediaQuery[cond + `Js`])
+    const [match, setMatch] = useState(query.matches)
+    useEffect(() => {
+      const handleMatch = q => setMatch(q.matches)
+      query.addListener(handleMatch)
+      return () => query.removeListener(handleMatch)
+    })
+    return match
+  }
+}
