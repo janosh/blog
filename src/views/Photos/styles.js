e3a53ef24f1a91619f40e2da7f23aef47d2b0bb7
janosh <janosh.riebesell@gmail.com>
2019-04-13 21:51:20 +0100

    refactor: Photos, Projects view usage of Modal

--- src/views/Photos/styles.js
+++ src/views/Photos/styles.js
@@ -1,23 +1,14 @@
 import styled from "styled-components"
 import Img from "gatsby-image"
 
+export { Img }
+
 export const Thumbnail = styled(Img)`
   border-radius: ${props => props.theme.mediumBorderRadius};
   transition: ${props => props.theme.shortTrans};
   height: 100%;
+  cursor: pointer;
   :hover {
     transform: scale(1.05);
   }
-`
-
-export const LargeImg = styled(Img).attrs(
-  props =>
-    props.fluid.aspectRatio < 1.45 && {
-      imgStyle: { objectFit: `contain` },
-    }
-)`
-  width: 80vw;
-  background: rgba(0, 0, 0, 0.9);
-  height: max-content;
-  max-height: 80vh;
 `
