eeb7ff2821b0571b22dec27dae7541be41b8ca56
janosh <janosh.riebesell@gmail.com>
2019-04-13 21:51:57 +0100

    fix: broken cover fragment after Gatsby schema update

--- src/utils/fragments/cover.js
+++ src/utils/fragments/cover.js
@@ -1,18 +1,24 @@
 import { graphql } from "gatsby"
 
 export const query = graphql`
-  fragment cover on frontmatter_2 {
+  fragment sharpSrc on File {
+    sharp: childImageSharp {
+      fluid(maxWidth: 1800) {
+        ...GatsbyImageSharpFluid_withWebp
+      }
+    }
+    src: publicURL
+  }
+  fragment cover on MarkdownRemarkFrontmatter {
     cover {
       credit
       url
       caption
       img {
-        sharp: childImageSharp {
-          fluid(maxWidth: 1800) {
-            ...GatsbyImageSharpFluid_withWebp
-          }
-        }
-        src: publicURL
+        ...sharpSrc
+      }
+      thumbnail {
+        ...sharpSrc
       }
       backdrop
     }
