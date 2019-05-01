50bee9156165c3ffbfe06f9b7b7df351df8c1317
janosh <janosh.riebesell@gmail.com>
2019-05-01 21:13:34 +0100

    feat: katex add macros for mathbb and mathcal, remove argmin, argmax (introduced as part of katex 10.1)

--- src/utils/katex.js
+++ src/utils/katex.js
@@ -1,10 +1,18 @@
 const macros = {
-  "\\expec": `\\langle #1\\rangle`,
-  "\\dif": `\\mathrm d`,
-  "\\argmin": `\\operatorname{argmin}`,
-  "\\argmax": `\\operatorname{argmax}`,
-  "\\vec": `{\\boldsymbol{#1}}`,
-  "\\mat": `{\\boldsymbol{#1}}`,
+  '\\dif': `\\mathrm d`,
+  '\\vec': `{\\boldsymbol{#1}}`,
+  '\\mat': `{\\boldsymbol{#1}}`,
+  '\\reals': `{\\mathbb{R}}`,
+  '\\comps': `{\\mathbb{C}}`,
+  '\\ints': `{\\mathbb{Z}}`,
+  '\\expec': `\\mathbb{E}`,
+  '\\varexpec': `\\langle #1\\rangle`,
+}
+
+for (let i = `A`.charCodeAt(); i <= `Z`.charCodeAt(); i++) {
+  const letter = String.fromCharCode(i)
+  macros[`\\${letter}cal`] = `\\mathcal{${letter}}`
+  macros[`\\${letter}bb`] = `\\mathbb{${letter}}`
 }
 
 module.exports = macros
