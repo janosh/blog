85f6a6ac03a38e9f868252ee612f670dd8d876d3
janosh <janosh.riebesell@gmail.com>
2019-05-11 17:29:40 +0100

    refactor: MobileNav and Search outsource useClickOutside hook

--- src/components/Nav/Mobile/index.js
+++ src/components/Nav/Mobile/index.js
@@ -1,8 +1,9 @@
-import React, { memo, useState, useRef, useEffect } from "react"
-import { useSpring, animated } from "react-spring"
+import React, { memo, useEffect, useRef, useState } from "react"
+import { animated, useSpring } from "react-spring"
 import ResizeObserver from "resize-observer-polyfill"
 
-import { Item, Children, Icons, MobileNavDiv, Menu, NavLink } from "./styles"
+import { useClickOutside } from "../../../utils/hooks"
+import { Children, Icons, Item, Menu, MobileNavDiv, NavLink } from "./styles"
 
 export const useSize = (ref, quantity) => {
   const [size, setSize] = useState(0)
@@ -48,23 +49,10 @@ const Tree = memo(({ text, url, children }) => {
 export default function MobileNav({ nav }) {
   const ref = useRef()
   const [open, setOpen] = useState(false)
-
-  const toggleNav = () => setOpen(!open)
-
-  const handleClickOutside = event =>
-    !ref.current.contains(event.target) && open && toggleNav()
-
-  useEffect(() => {
-    document.addEventListener(`mousedown`, handleClickOutside)
-    document.addEventListener(`touchstart`, handleClickOutside)
-    return () => {
-      document.removeEventListener(`mousedown`, handleClickOutside)
-      document.removeEventListener(`touchstart`, handleClickOutside)
-    }
-  })
+  useClickOutside(ref, () => open && setOpen(false))
   return (
     <>
-      <Menu onClick={toggleNav} />
+      <Menu onClick={() => setOpen(!open)} />
       <MobileNavDiv ref={ref} open={open} onScroll={e => e.preventDefault()}>
         {nav.map(({ title, url, subNav }) => (
           <Tree key={url} url={url} text={title}>
