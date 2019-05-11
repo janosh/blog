38ddbe89c98bef4208f51bf0d8f45b13025ad6a7
janosh <janosh.riebesell@gmail.com>
2019-05-11 17:29:40 +0100

    refactor: MobileNav and Search outsource useClickOutside hook

---
+++ src/utils/hooks.js
@@ -0,0 +1,15 @@
+import { useEffect } from 'react'
+
+export const useClickOutside = (ref, handler, events) => {
+  if (!events) events = [`mousedown`, `touchstart`]
+  const detectClickOutside = event =>
+    !ref.current.contains(event.target) && handler()
+  useEffect(() => {
+    for (const event of events)
+      document.addEventListener(event, detectClickOutside)
+    return () => {
+      for (const event of events)
+        document.removeEventListener(event, detectClickOutside)
+    }
+  })
+}
