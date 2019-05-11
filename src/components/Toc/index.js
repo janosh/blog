48dc725bb35d4bc04639880e7b5b67c534a0b786
janosh <janosh.riebesell@gmail.com>
2019-05-11 17:37:56 +0100

    feat: add Toc comp that lists markdown headings for longer posts/pages

---
+++ src/components/Toc/index.js
@@ -0,0 +1,35 @@
+import React, { useRef, useState } from 'react'
+
+import { useClickOutside } from '../../utils/hooks'
+import { TocDiv, TocLink, TocIcon, Title, Toggle } from './styles'
+
+export default function Toc({ headings, title = `Contents` }) {
+  const [open, setOpen] = useState(false)
+  const minDepth = Math.min(...headings.map(({ depth }) => depth))
+  const ref = useRef()
+  useClickOutside(ref, () => setOpen(false))
+  return (
+    <>
+      <Toggle opener onClick={() => setOpen(true)} size="1.6em" />
+      <TocDiv ref={ref} open={open}>
+        <Title open={open} css="margin: 0;">
+          <TocIcon />
+          {title}
+          <Toggle closer onClick={() => setOpen(false)} />
+        </Title>
+        <nav open={open}>
+          {headings.map(({ value, depth, slug }) => (
+            <TocLink
+              key={value}
+              depth={depth - minDepth}
+              to={slug}
+              onClick={() => setOpen(false)}
+            >
+              {value}
+            </TocLink>
+          ))}
+        </nav>
+      </TocDiv>
+    </>
+  )
+}
---
+++ src/components/Toc/styles.js
@@ -0,0 +1,88 @@
+import styled, { css } from 'styled-components'
+import { Link } from 'gatsby'
+
+import { Close as Cross } from 'styled-icons/material/Close'
+import { BookContent } from 'styled-icons/boxicons-regular/BookContent'
+
+import mediaQuery from '../../utils/mediaQuery'
+
+const openTocDiv = css`
+  background: white;
+  padding: 0.7em 1.2em;
+  border-radius: 0.5em;
+  border: 1px solid ${props => props.theme.lightGray};
+`
+
+export const TocDiv = styled.div`
+  height: max-content;
+  max-height: 80vh;
+  overflow-y: scroll;
+  z-index: 1;
+  line-height: 2.2em;
+  -webkit-overflow-scrolling: touch;
+  ${mediaQuery.maxLaptop} {
+    max-width: 16em;
+    position: fixed;
+    bottom: 1em;
+    left: 1em;
+    ${props => !props.open && `height: 0;`};
+    ${props => props.open && openTocDiv};
+    visibility: ${props => (props.open ? `visible` : `hidden`)};
+    opacity: ${props => (props.open ? 1 : 0)};
+    transition: ${props => props.theme.shortTrans};
+  }
+  ${mediaQuery.minLaptop} {
+    max-width: 15em;
+    font-size: 0.85em;
+    grid-column: 4 / -1;
+    position: sticky;
+    top: 2em;
+    right: 2em;
+  }
+`
+
+export const Title = styled.h2`
+  padding-bottom: 0.5em;
+  display: grid;
+  grid-auto-flow: column;
+  align-items: center;
+  grid-template-columns: auto auto 1fr;
+`
+
+export const TocLink = styled(Link)`
+  display: block;
+  margin-left: ${props => props.depth + `em`};
+  border-top: ${props =>
+    props.depth === 0 && `1px solid ` + props.theme.lightGray};
+`
+
+export const TocIcon = styled(BookContent)`
+  width: 1em;
+  margin-right: 0.2em;
+`
+
+const openerCss = `
+  position: fixed;
+  bottom: 1em;
+  left: 1em;
+`
+
+const closerCss = css`
+  background: ${props => props.theme.lightGray};
+  border-radius: 50%;
+`
+
+export const Toggle = styled(Cross).attrs(props => ({
+  as: props.opener && BookContent,
+  size: props.size || `1.4em`,
+}))`
+  transition: ${props => props.theme.shortTrans};
+  justify-self: end;
+  :hover {
+    transform: scale(1.1);
+  }
+  ${mediaQuery.minLaptop} {
+    display: none;
+  }
+  ${props => (props.opener ? openerCss : closerCss)};
+`
