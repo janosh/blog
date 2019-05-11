87eb3785ba0e3203ff2f90c705d9e7b7cac3195a
janosh <janosh.riebesell@gmail.com>
2019-05-11 17:36:47 +0100

    fix: katex \dif macro spacing, .vscode-highlight-line-highlighted left border, typography sizes, graphql date formatting, theme darken "blue"

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,8 +1,8 @@
 const typography = {
   fonts: `-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif`,
   // font sizes and line heigths in em units
-  minFontSize: 1,
-  maxFontSize: 1.2,
+  minFontSize: 0.9,
+  maxFontSize: 1.1,
   minLineHeight: 1.5,
   maxLineHeight: 1.8,
 }
