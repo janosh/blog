b3f77b0cb3a4b57f642cd0a665f617193d5d3caf
janosh <janosh.riebesell@gmail.com>
2019-05-11 17:33:52 +0100

    feat: PageBody use grid-auto-flow: dense

--- src/components/styles/PageBody.js
+++ src/components/styles/PageBody.js
@@ -1,13 +1,15 @@
-import styled from "styled-components"
+import styled from 'styled-components'
 
-import mediaQuery from "../../utils/mediaQuery"
+import mediaQuery from '../../utils/mediaQuery'
 
 export const PageBody = styled.main`
+  position: relative;
   margin: calc(3em + 3vh) 0;
   display: grid;
   grid-gap: 0 4vw;
   grid-template-columns: 1fr 1fr minmax(8em, ${props => props.theme.maxWidth}) 1fr 1fr;
   grid-auto-rows: max-content;
+  grid-auto-flow: dense;
   > * {
     grid-column: ${props => props.cols || 3};
   }
