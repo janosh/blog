eb8464a73f819cc22c1ebf5cca677fb149220d94
janosh <janosh.riebesell@gmail.com>
2019-06-12 20:15:37 +0100

    chore: yarn add eslint-plugin-react-hooks and address all errors and warnings

--- src/hooks/useClickOutside.js
+++ src/hooks/useClickOutside.js
@@ -2,14 +2,14 @@ import { useEffect } from "react"
 
 export const useClickOutside = (ref, handler, events) => {
   if (!events) events = [`mousedown`, `touchstart`]
-  const detectClickOutside = event =>
-    !ref.current.contains(event.target) && handler()
   useEffect(() => {
+    const detectClickOutside = event =>
+      !ref.current.contains(event.target) && handler()
     for (const event of events)
       document.addEventListener(event, detectClickOutside)
     return () => {
       for (const event of events)
         document.removeEventListener(event, detectClickOutside)
     }
-  })
+  }, [events, handler, ref])
 }
