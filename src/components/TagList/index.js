3d89f183a8df971c710096fb9d4bf11ad2d14cc1
janosh <janosh.riebesell@gmail.com>
2019-06-16 13:05:12 +0200

    refactor: BlogPage with useQueryParam hook

--- src/components/TagList/index.js
+++ src/components/TagList/index.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
-import PropTypes from 'prop-types'
+import React, { useState } from "react"
+import PropTypes from "prop-types"
 
-import { TagGrid, Toggle, Tag, TagsIcon, tagIcons } from './styles'
+import { TagGrid, Toggle, Tag, TagsIcon, tagIcons } from "./styles"
 
-export default function TagList({ tags, activeTag, setTag }) {
+export default function TagList({ tags, activeTag = `all`, setActiveTag }) {
   const [open, setOpen] = useState(false)
   return (
     <TagGrid open={open}>
@@ -12,14 +12,14 @@ export default function TagList({ tags, activeTag, setTag }) {
         &nbsp; Tags
         <Toggle open={open} onClick={() => setOpen(!open)} />
       </h2>
-      {tags.map(({ title, count }) => {
+      {tags.map(({ title, slug, count }) => {
         const TagIcon = tagIcons[title]
         return (
           <Tag
             open={open}
             key={title}
-            active={activeTag === title}
-            onClick={() => setTag(title)}
+            active={activeTag === slug}
+            onClick={() => setActiveTag(slug === `all` ? null : slug)}
           >
             {TagIcon && <TagIcon size="1em" />}
             &nbsp; {title} ({count})
@@ -31,8 +31,8 @@ export default function TagList({ tags, activeTag, setTag }) {
 }
 
 TagList.propTypes = {
-  activeTag: PropTypes.string.isRequired,
-  setTag: PropTypes.func.isRequired,
+  activeTag: PropTypes.string,
+  setActiveTag: PropTypes.func.isRequired,
   tags: PropTypes.arrayOf(
     PropTypes.shape({
       title: PropTypes.string.isRequired,
