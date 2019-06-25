c8a17ff3c8b5faf603a7035c7e2998c503566a4e
janosh <janosh.riebesell@gmail.com>
2019-06-25 09:09:29 +0200

    fix: Toc comp useEffect deps caused infinite loop

--- src/components/Toc/index.js
+++ src/components/Toc/index.js
@@ -12,11 +12,7 @@ const accumulateOffsetTop = (el, totalOffset = 0) => {
   return totalOffset
 }
 
-export default function Toc({
-  headingSelector = Array.from({ length: 6 }, (_, i) => `main h` + (i + 1)),
-  title = `Contents`,
-  getDepth = node => Number(node.nodeName[1]),
-}) {
+export default function Toc({ headingSelector, getTitle, getDepth, tocTitle }) {
   const [headings, setHeadings] = useState({
     titles: [],
     nodes: [],
@@ -27,18 +23,29 @@ export default function Toc({
   const ref = useRef()
   useOnClickOutside(ref, () => setOpen(false))
   useEffect(() => {
-    const nodes = Array.from(document.querySelectorAll(headingSelector))
+    // Fallback to sensible defaults for headingSelector, getTitle and getDepth
+    // inside useEffect rather than specifying them as Toc default props to avoid
+    // the need for useMemo and useCallback, resp.
+    // Otherwise, these would change on every render and since this effect calls
+    // setHeadings which triggers a rerender, it would cause an infinite loop.
+
+    const selector =
+      headingSelector || Array.from({ length: 6 }, (_, i) => `main h` + (i + 1))
+    const nodes = Array.from(document.querySelectorAll(selector))
     const titles = nodes.map(node => ({
-      title: node.innerText,
-      depth: getDepth(node),
+      title: getTitle ? getTitle(node) : node.innerText,
+      depth: getDepth ? getDepth(node) : Number(node.nodeName[1]),
     }))
     const minDepth = Math.min(...titles.map(h => h.depth))
     setHeadings({ titles, nodes, minDepth })
+  }, [headingSelector, getTitle, getDepth])
 
+  useEffect(() => {
     const scrollHandler = throttle(() => {
-      // Offsets need to be recomputed because lazily-loaded content
-      // increases increases offsets as user scrolls down.
-      const offsets = headings.nodes.map(el => accumulateOffsetTop(el))
+      const { titles, nodes } = headings
+      // Offsets need to be recomputed because lazily-loaded
+      // content increases offsets as user scrolls down.
+      const offsets = nodes.map(el => accumulateOffsetTop(el))
       const activeIndex = offsets.findIndex(
         offset => offset > window.scrollY + 0.8 * window.innerHeight
       )
@@ -47,7 +54,7 @@ export default function Toc({
 
     window.addEventListener(`scroll`, scrollHandler)
     return () => window.removeEventListener(`scroll`, scrollHandler)
-  }, [getDepth, headingSelector, headings])
+  }, [headings])
 
   return (
     <>
@@ -55,7 +62,7 @@ export default function Toc({
       <TocDiv ref={ref} open={open}>
         <Title>
           <TocIcon />
-          {title}
+          {tocTitle || `Contents`}
           <Toggle closer onClick={() => setOpen(false)} />
         </Title>
         <nav>
