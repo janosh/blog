fafc2eb7bca3d273a82983954e4b50c27581ffa5
janosh <janosh.riebesell@gmail.com>
2019-09-07 11:32:21 +0100

    refactor: Masonry, Modal, PageTitle, Scroll, Toc comps and useOnClickOutside hook to use useEventListener hook

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -2,28 +2,25 @@
 import PropTypes from 'prop-types'
 import React, { useEffect, useState } from 'react'
 import { ThemeProvider } from 'styled-components'
+import { useEventListener } from '../../hooks'
 import { controls, ModalBehind, ModalDiv } from './styles'
 
 const { Close, Next, Prev, FullscreenToggle } = controls
 
-const handleArrowKeys = (modal, setModal) => event => {
-  if (event.key === `ArrowRight`) setModal(modal + 1)
-  else if (event.key === `ArrowLeft`) setModal(modal - 1)
+const handleArrowKeys = setModal => event => {
+  if (event && event.key === `ArrowRight`) setModal(m => m + 1)
+  else if (event && event.key === `ArrowLeft`) setModal(m => m - 1)
 }
 
 export default function Modal({ open, modal, setModal, children, ...rest }) {
   if (open) {
     const { showControls = true, className, whiteControls } = rest
     const [fullscreen, setFullscreen] = useState(rest.fullScreenDefault)
+    useEventListener(`keydown`, handleArrowKeys(setModal))
     useEffect(() => {
       document.body.style.overflowY = `hidden`
-      const handler = handleArrowKeys(modal, setModal)
-      document.addEventListener(`keydown`, handler)
-      return () => {
-        document.removeEventListener(`keydown`, handler)
-        document.body.style.removeProperty(`overflow-y`)
-      }
-    }, [modal, setModal])
+      return () => document.body.style.removeProperty(`overflow-y`)
+    }, [])
     return (
       // calling setModal without arguments will close the modal
       <ModalBehind open={open} onClick={setModal}>
