7c2d5bf69c01131b389a8fac2de80306dbd1a0fc
janosh <janosh.riebesell@gmail.com>
2019-10-30 20:20:04 +0000

    fix: breaking changes in react-instantsearch v6

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,11 +1,6 @@
 import algoliasearch from 'algoliasearch/lite'
 import React, { createRef, useMemo, useState } from 'react'
-import {
-  connectStateResults,
-  Hits,
-  Index,
-  InstantSearch,
-} from 'react-instantsearch-dom'
+import { connectStateResults, Hits, Index, InstantSearch } from 'react-instantsearch-dom'
 import { useOnClickOutside } from 'hooks'
 import * as hitComps from './hitComps'
 import Input from './Input'
@@ -25,39 +20,35 @@ export default function Search({ indices, collapse, hitsAsGrid }) {
   const ref = createRef()
   const [query, setQuery] = useState(``)
   const [focus, setFocus] = useState(false)
+  const appId = process.env.GATSBY_ALGOLIA_APP_ID
+  const searchKey = process.env.GATSBY_ALGOLIA_SEARCH_KEY
   // useMemo prevents the searchClient from being recreated on every render.
   // Avoids unnecessary XHR requests (see https://tinyurl.com/yyj93r2s).
-  const searchClient = useMemo(
-    () =>
-      algoliasearch(
-        process.env.GATSBY_ALGOLIA_APP_ID,
-        process.env.GATSBY_ALGOLIA_SEARCH_KEY
-      ),
-    []
-  )
+  const searchClient = useMemo(() => algoliasearch(appId, searchKey), [appId, searchKey])
   useOnClickOutside(ref, () => setFocus(false))
   return (
-    <InstantSearch
-      searchClient={searchClient}
-      indexName={indices[0].name}
-      onSearchStateChange={({ query }) => setQuery(query)}
-      root={{ Root, props: { ref } }}
-    >
-      <Input onFocus={() => setFocus(true)} {...{ collapse, focus }} />
-      <HitsWrapper show={query.length > 0 && focus} asGrid={hitsAsGrid}>
-        {indices.map(({ name, title, hitComp }) => (
-          <Index key={name} indexName={name}>
-            <header>
-              <h3>{title}</h3>
-              <Stats />
-            </header>
-            <Results>
-              <Hits hitComponent={hitComps[hitComp](() => setFocus(false))} />
-            </Results>
-          </Index>
-        ))}
-        <PoweredBy />
-      </HitsWrapper>
-    </InstantSearch>
+    <Root ref={ref}>
+      <InstantSearch
+        searchClient={searchClient}
+        indexName={indices[0].name}
+        onSearchStateChange={({ query }) => setQuery(query)}
+      >
+        <Input onFocus={() => setFocus(true)} {...{ collapse, focus }} />
+        <HitsWrapper show={query.length > 0 && focus} asGrid={hitsAsGrid}>
+          {indices.map(({ name, title, hitComp }) => (
+            <Index key={name} indexName={name}>
+              <header>
+                <h3>{title}</h3>
+                <Stats />
+              </header>
+              <Results>
+                <Hits hitComponent={hitComps[hitComp](() => setFocus(false))} />
+              </Results>
+            </Index>
+          ))}
+          <PoweredBy />
+        </HitsWrapper>
+      </InstantSearch>
+    </Root>
   )
 }
