eb0e7a846a30b5a5876f404143c8699017cbf17b
janosh <janosh.riebesell@gmail.com>
2019-11-24 11:09:45 +0000

    chore: remove Social from Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import DarkMode from '../DarkMode'
-import Nav, { navLinkStyle } from '../Nav'
+import Nav from '../Nav'
 import Search from '../Search'
-import Social from '../Social'
 import { HeaderContainer, Logo } from './styles'
 
 const searchIndices = [
@@ -10,16 +9,15 @@ const searchIndices = [
   { name: `Posts`, title: `Blog Posts`, type: `postHit` },
 ]
 
-const Header = ({ site }) => (
-  <HeaderContainer>
-    <Logo to="/" title={site.title} rel="home">
-      JR
-    </Logo>
-    <Nav />
-    <DarkMode />
-    <Social collapse short linkStyle={navLinkStyle} />
-    <Search collapse indices={searchIndices} />
-  </HeaderContainer>
-)
-
-export default Header
+export default function Header({ site }) {
+  return (
+    <HeaderContainer>
+      <Logo to="/" title={site.title} rel="home">
+        JR
+      </Logo>
+      <Nav />
+      <DarkMode />
+      <Search indices={searchIndices} />
+    </HeaderContainer>
+  )
+}
