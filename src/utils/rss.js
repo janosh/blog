649e907aed86dda78f258d1693c1933cb7ab9f90
Janosh Riebesell <janosh.riebesell@gmail.com>
2020-01-20 17:15:48 +0000

    feat: add RSS feed

---
+++ src/utils/rss.js
@@ -0,0 +1,49 @@
+// gatsby-plugin-feed-mdx config
+module.exports = {
+  query: `{
+    site {
+      siteMetadata {
+        url
+        site_url: url
+      }
+    }
+  }`,
+  feeds: [
+    {
+      serialize: ({ query }) => {
+        const { url } = query.site.siteMetadata
+        return query.posts.edges.map(({ node }) => {
+          const { slug } = node.frontmatter
+          return {
+            ...node.frontmatter,
+            description: node.excerpt,
+            url: url + slug,
+            guid: url + slug,
+            custom_elements: [{ 'content:encoded': node.html }],
+          }
+        })
+      },
+      query: `{
+        posts: allMdx(
+          filter: { fileAbsolutePath: { regex: "/posts/" } }
+          sort: { fields: frontmatter___date, order: DESC }
+        ) {
+          edges {
+            node {
+              frontmatter {
+                title
+                slug
+                date(formatString: "MMM D, YYYY")
+              }
+              timeToRead
+              excerpt(pruneLength: 300)
+              html
+            }
+          }
+        }
+      }`,
+      title: `janosh.io`,
+      output: `/rss.xml`,
+    },
+  ],
+}
