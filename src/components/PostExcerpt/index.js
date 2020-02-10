e9aa1f70637f0bc9fd67ab6cbf38a7172aa3d46f
Janosh Riebesell <janosh.riebesell@gmail.com>
2020-02-10 09:29:08 +0000

    chore: add image alt tags to improve lighthouse seo score

--- src/components/PostExcerpt/index.js
+++ src/components/PostExcerpt/index.js
@@ -6,15 +6,10 @@ import { Cover, Post } from './styles'
 export default function PostExcerpt({ post, noText }) {
   const { frontmatter, excerpt, timeToRead } = post
   const { title, slug, cover } = frontmatter
-  if (cover && cover.img) {
-    const img = cover.thumbnail || cover.img
-    if (img.sharp) cover.fluid = img.sharp.fluid
-    if (img.src) cover.src = img.src
-  }
   return (
     <Post>
       <Link to={slug}>
-        <Cover {...cover} />
+        <Cover {...cover} {...cover.img} />
       </Link>
       <h3 css="margin: 0.8em auto 0.5em;">
         <Link to={slug}>{title}</Link>
