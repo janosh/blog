f24df1d15a9ae9f32d3367706c398e033e66317e
Janosh Riebesell <janosh.riebesell@gmail.com>
2020-02-10 09:29:08 +0000

    chore: add image alt tags to improve lighthouse seo score

--- src/views/PostList/index.js
+++ src/views/PostList/index.js
@@ -1,18 +1,13 @@
-import PropTypes from 'prop-types'
 import React from 'react'
 import PostExcerpt from 'components/PostExcerpt'
 import { PostGrid } from './styles'
 
-const PostList = ({ posts, noText, ...rest }) => (
-  <PostGrid minWidth="17em" maxWidth="24em" gap="1.5em" {...rest}>
-    {posts.map(({ node }) => (
-      <PostExcerpt key={node.frontmatter.slug} post={node} noText={noText} />
-    ))}
-  </PostGrid>
-)
-
-export default PostList
-
-PostList.propTypes = {
-  posts: PropTypes.arrayOf(PropTypes.object),
+export default function PostList({ posts, noText, ...rest }) {
+  return (
+    <PostGrid minWidth="17em" maxWidth="24em" gap="1.5em" {...rest}>
+      {posts.map(({ node }) => (
+        <PostExcerpt key={node.frontmatter.slug} post={node} noText={noText} />
+      ))}
+    </PostGrid>
+  )
 }
