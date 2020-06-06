da8f2d3435595e6b5faa03038d29d0fb255b9d40
Janosh Riebesell <janosh.riebesell@gmail.com>
2020-06-06 15:48:36 +0200

    chore: update eslint env to es2020

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,53 +4,46 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true,
+    es2020: true,
     node: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:mdx/recommended',
-  ],
-  parser: 'babel-eslint',
+  extends: [`eslint:recommended`, `plugin:react/recommended`],
+  parser: `babel-eslint`,
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: `module`,
   },
-  plugins: ['react', 'react-hooks', 'markdown'],
+  plugins: [`react`, `react-hooks`, `markdown`],
   rules: {
-    indent: ['error', 2, { SwitchCase: 1 }],
-    quotes: ['error', 'backtick', { avoidEscape: true }],
-    semi: ['error', 'never'],
-    'linebreak-style': ['error', 'unix'],
-    'react/prop-types': 'off',
-    'react/display-name': 'off',
-    'no-console': ['error', { allow: ['warn', 'error'] }],
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
+    indent: [`error`, 2, { SwitchCase: 1 }],
+    quotes: [`error`, `backtick`, { avoidEscape: true }],
+    semi: [`error`, `never`],
+    'linebreak-style': [`error`, `unix`],
+    'react/prop-types': `off`,
+    'react/display-name': `off`,
+    'no-console': [`error`, { allow: [`warn`, `error`] }],
+    'react-hooks/rules-of-hooks': `error`,
+    'react-hooks/exhaustive-deps': `warn`,
+    'no-var': `error`,
+    'spaced-comment': [`error`, `always`],
   },
   overrides: [
     {
-      files: ['*.md'],
+      files: [`*.md`],
       rules: {
-        'no-undef': 'off',
-        'no-unused-vars': 'off',
-        'react/jsx-no-undef': 'off',
-        'react/react-in-jsx-scope': 'off',
-        'react/jsx-no-comment-textnodes': 'off',
+        'no-undef': `off`,
+        'no-unused-vars': `off`,
+        'react/jsx-no-undef': `off`,
+        'react/react-in-jsx-scope': `off`,
+        'react/jsx-no-comment-textnodes': `off`,
       },
     },
   ],
   settings: {
     react: {
-      version: 'detect',
+      version: `detect`,
     },
-  },
-  globals: {
-    globalThis: false, // false means it is not writeable.
-    // See https://github.com/eslint/eslint/issues/11553.
   },
 }
