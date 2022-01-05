d5d8b843e0fbe36dfd5ad77758f08133b9354535
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-01-05 17:18:56 +0100

    del gatsby/react stuff, keep only content

--- src/hooks/useOnClickOutside.js
+++
@@ -1,8 +0,0 @@
-import { useEventListener } from '.'
-
-export function useOnClickOutside(ref, handler, events) {
-  if (!events) events = [`mousedown`, `touchstart`]
-  const detectClickOutside = event =>
-    ref.current && event && !ref.current.contains(event.target) && handler(event)
-  useEventListener(events, detectClickOutside)
-}
