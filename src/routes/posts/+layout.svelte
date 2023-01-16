c2c8125f2034f430318f18f3190737b29775ef14
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-01-15 17:24:19 -0800

    migrate from gatsby to sveltekit

---
+++ src/routes/posts/+layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import type { LayoutServerData } from '../physics/$types'
+
+  export let data: LayoutServerData
+
+  const routes = Object.keys(import.meta.glob('./*/+page*.{svx,md,svelte}')).map(
+    (filename) => filename.split(`/`)[1]
+  )
+
+  $: ({ src, caption } = data.frontmatter.cover)
+</script>
+
+<img {src} alt={caption} />
+<h1>{data.frontmatter.title}</h1>
+
+<main>
+  <slot />
+</main>
+
+<style>
+  img {
+    margin: 0;
+    height: 50vh;
+    object-fit: cover;
+  }
+</style>
