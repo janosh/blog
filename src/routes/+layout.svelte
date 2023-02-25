a8cbb8152c62e3b025e21a54e9ea06332572bb76
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-02-25 15:28:03 -0800

    fix PrevNext data loading, add index page link to layout and PrevNext

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,26 @@
 <script lang="ts">
+  import { page } from '$app/stores'
   import { Footer } from '$lib'
   import '../app.css'
-
-  // const routes = Object.keys(import.meta.glob(`./*/+page*.{svx,md,svelte}`)).map(
-  //   (filename) => filename.split(`/`)[1]
-  // )
 </script>
 
-<!-- <Nav {routes} /> -->
+{#if $page.url.pathname !== `/`}
+  <a href="/" aria-label="Back to index page">&laquo; home</a>
+{/if}
 
 <slot />
 
 <Footer />
+
+<style>
+  a[href='/'] {
+    font-size: 15pt;
+    position: absolute;
+    top: 2em;
+    left: 2em;
+    background-color: rgba(0, 0, 0, 0.5);
+    padding: 1pt 5pt;
+    border-radius: 3pt;
+    transition: 0.2s;
+  }
+</style>
