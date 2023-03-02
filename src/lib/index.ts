1ac0c3e92d870308235d9621a8d38c78c90f6a7c
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-03-02 12:28:15 -0800

    add cmd palette for keyboard-only site navigation (uses svelte-multiselect)
    clean up server code for post loading
    del PrevNext.svelte, replaced with svelte-zoo component
    update deps incl. TS v5 to use moduleResolution='bundler'

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,3 @@
 export { default as DocsGrid } from './DocsGrid.svelte'
 export { default as Footer } from './Footer.svelte'
 export { default as Nav } from './Nav.svelte'
-export { default as PrevNext } from './PrevNext.svelte'
--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,1 +1,17 @@
 export const prerender = true
+
+export const load = async ({ url }) => {
+  const posts = Object.entries(
+    // eslint-disable-next-line @typescript-eslint/quotes
+    import.meta.glob('./posts/*/+page.{md,svx,svelte}', { eager: true })
+  ).map(([file, post]) => ({
+    ...post.metadata,
+    slug: file.split(`/`)[2],
+    path: '/' + file.split(`/`).slice(1, -1).join(`/`),
+    file,
+  }))
+
+  const post = posts.find((post) => post.path === url.pathname)
+
+  return { posts, post }
+}
