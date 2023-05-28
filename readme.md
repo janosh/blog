52cf671fe26662b3e0df656522c02ce0e82c1cc7
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-05-28 08:18:18 -0700

    add landing page screen shot to readme
    link repo from footer

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
-# My Blog
+# [My Blog](https://janosh.dev)
 
-I write/plan to write about physics, machine learning, sustainability and web development.
+I write about things I learned in machine learning, data visualization, web development and coding best practices.
+
+![Landing Page](https://github.com/janosh/blog/assets/30958850/962918ab-f9f7-470b-a605-ebbfa11461b2)
--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { repository as repo } from '$root/package.json'
   import Icon from '@iconify/svelte'
 </script>
 
@@ -6,7 +7,19 @@
   <Icon icon="material-symbols:arrow-upward-rounded" />
 </button>
 
+<footer>
+  <a href={repo}><Icon icon="octicon:mark-github" width="20pt" />Source</a>
+</footer>
+
 <style>
+  footer {
+    margin: 3em;
+  }
+  footer a {
+    display: flex;
+    place-content: center;
+    gap: 5pt;
+  }
   button {
     position: absolute;
     bottom: 18pt;
