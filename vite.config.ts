80a04d57e8c6c3decd79d5cf5831e9fdf470b209
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-10-08 07:16:17 -0700

    use GH API to fetch up-to-date stargazers_count in cv/+page.server.ts
    add CHGNet, atomate2, jobflow to cv.projects

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,41 @@
 import yaml from '@rollup/plugin-yaml'
 import { sveltekit } from '@sveltejs/kit/vite'
-import type { UserConfig } from 'vite'
+import fs from 'fs'
+import yamljs from 'js-yaml'
+import { loadEnv, type UserConfig } from 'vite'
+
+async function fetch_github_data(gh_token: string) {
+  const auth = { headers: { Authorization: `token ${gh_token}` } }
+  const cv = yamljs.load(fs.readFileSync(`src/routes/cv/cv.yml`, `utf8`))
+
+  for (const project of cv.projects) {
+    const handle = project.github.replace(`https://github.com/`, ``)
+
+    const repo_promise = await fetch(
+      `https://api.github.com/repos/${handle}`,
+      auth,
+    )
+
+    const repo = await repo_promise.json()
+    project.stars = repo.stargazers_count
+
+    const languages = await (
+      await fetch(`https://api.github.com/repos/${handle}/languages`, auth)
+    ).json()
+    project.languages = Object.keys(languages)
+  }
+
+  fs.writeFileSync(`src/routes/cv/cv.yml`, yamljs.dump(cv, { lineWidth: -1 }))
+}
+
+process.env = { ...process.env, ...loadEnv(``, process.cwd(), ``) }
+const { gh_token } = process.env
+
+if (gh_token) {
+  fetch_github_data(gh_token)
+} else {
+  console.error(`No GitHub token found, skipping GitHub API calls`)
+}
 
 export default {
   plugins: [sveltekit(), yaml()],
