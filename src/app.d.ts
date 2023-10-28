fd5c580cbf34856cfc61019643e46ffaba780707
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-10-27 20:21:03 -0700

    declare module '*cv.yml'

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,3 +3,20 @@
 
 declare module '*.md'
 declare module '*package.json'
+
+declare module '*cv.yml' {
+  import type * as types from '$lib/types'
+  const cv: {
+    education: types.Education[]
+    publications: types.Publication[]
+    projects: types.Project[]
+    skills: types.Skill[]
+    hobbies: types.Hobby[]
+    nationality: string[]
+    languages: types.Language[]
+    volunteer: types.Volunteer[]
+    awards: types.Award[]
+    memberships: types.Membership[]
+  }
+  export default cv
+}
